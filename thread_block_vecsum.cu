e5180b1082684343b374cc80b4538e5ea7b8d417
ShreyasSkandan <shreyas.ibanez@gmail.com>
2016-10-08 13:50:23 -0400

    vector sum with threads and blocks complete

--- thread_block_vecsum.cu
+++ thread_block_vecsum.cu
@@ -1,6 +1,9 @@
 #include "/home/shreyas/CUDA_BY_EXAMPLE/common/book.h"
+#include <stdio.h>
 
 // Block limit is 65,535 ; Thread limit is 512
+
+#define N (25*256)
 
 __global__ void addvec_threads(int *vec1, int *vec2, int *sum)
 {
@@ -23,3 +26,47 @@ __global__ void addvec_threads(int *vec1, int *vec2, int *sum)
 }
 
 
+int main(void)
+{
+	FILE *fp;
+	char output[] = "thread_block_output.txt";
+
+	int vec1[N];
+	int vec2[N];
+	int sum[N];
+	int *device_v1, *device_v2, *device_sum;
+	
+	cudaMalloc((void**)&device_v1, N*sizeof(int));
+	cudaMalloc((void**)&device_v2, N*sizeof(int));
+	cudaMalloc((void**)&device_sum, N*sizeof(int));
+	
+	for (int iter=0;iter<N;iter++)
+	{
+		vec1[iter] = iter;
+		vec2[iter] = iter*iter;
+	}
+	
+	// CUDA MEMCPY format (destination,source,size,transfer_path)
+	// Prepare parameters for DEVICE computation
+	cudaMemcpy(device_v1,vec1,N*sizeof(int),cudaMemcpyHostToDevice);
+	cudaMemcpy(device_v2,vec2,N*sizeof(int),cudaMemcpyHostToDevice);
+	// Perform parallel computation on DEVICE
+	addvec_threads<<<128,128>>>(device_v1,device_v2,device_sum);
+	// Prepare result for output display on HOST
+	cudaMemcpy(sum, device_sum, N*sizeof(int), cudaMemcpyDeviceToHost);
+
+	// Write vector sum output to a FILE
+	fp = fopen(output,"w+");
+	for (int ii = 0; ii < N; ii++)
+	{
+	fprintf(fp,"%d\r\n",sum[ii]);
+	}
+	fclose(fp);
+	
+	cudaFree(device_v1);
+	cudaFree(device_v2);
+	cudaFree(device_sum);
+	return 0;
+	
+}
+	
