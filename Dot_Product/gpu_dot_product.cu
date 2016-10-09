6eec0a7157b4f397f7e143e96c1d447d684f066c
ShreyasSkandan <shreyas.ibanez@gmail.com>
2016-10-08 21:33:19 -0400

    gpu code for dot product

--- Dot_Product/gpu_dot_product.cu
+++ Dot_Product/gpu_dot_product.cu
@@ -7,7 +7,7 @@ const int N = 33 * 1024;
 #define min_value(a,b) (a<b?a:b)
 
 __global__ dotprod(float *vector1, float *vector2, float *result)
-{
+{ 
 	// Create a buffer on the GPU shared memory to store the intermediate sums
 	__shared__ float buffer[threads_per_block];
 	// Find thread index using block offset
@@ -24,6 +24,33 @@ __global__ dotprod(float *vector1, float *vector2, float *result)
 		// Increment thread element index by total number of elements in thread
 		thread_id = thread_id + blockDim.x * gridDim.x;
 	}
-	// Store this temporary sum of products in the shared memory buffer
+	// Store this temporary sum of products (running sum) in the shared memory buffer
 	buffer[buffer_idx] = temp_sum;
 
+	// Point of synchronisation for all above threaded computations
+	__syncthreads();
+
+	// REDUCTIONS -- Summation Reduction --
+	// Since we have 256 threads, to reduce this completely using two thread sum,
+	// we will need x:2^x=256 iterations (x=8 iterations)
+	int i = blockDim.x/2;
+	while (i!=0)
+	{
+		if(buffer_idx < i)
+		{
+			// Add i'th element of each thread in buffer
+			buffer[buffer_idx] += buffer[buffer_idx+i];
+		}
+		__syncthreads();
+		// i then reduces by half since 256 elements would become 128 after 
+		// the first reduction step
+		i = i/2;
+	}
+	if(buffer_idx == 0)
+	{
+		// Add thread sum to block result, return control to CPU to perform
+		// final arithmetic addition of block sums
+		result[blockIdx.x] = cache[0];
+	}
+}
+			
