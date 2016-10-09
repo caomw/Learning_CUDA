30578829814b57cd6999e4eaae1807620d1bfd5e
ShreyasSkandan <shreyas.ibanez@gmail.com>
2016-10-09 16:39:55 -0400

    find celebrity in matrix

---
+++ General_C/find_celebrity.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int matrix[5][5] = {{1,1,1,1,0},{1,1,1,0,0},{0,0,1,0,0},{1,0,1,1,1},{1,1,1,1,1}};
+	int row_sum_vec[5] = {};
+	int col_sum_vec[5] = {};
+	int n_people = 5;
+	int celeb_index = 2; // to verify
+
+	int i = 0;
+	int j = 0;
+
+	for(i=0;i<n_people;i++)
+	{
+		int row_sum = 0;
+		for(j=0;j<n_people;j++)
+		{	
+			row_sum += matrix[i][j];
+			printf("%d ",matrix[i][j]);
+			col_sum_vec[j] = col_sum_vec[j] + matrix[i][j];
+		}
+		printf("   sum = %d",row_sum);
+		row_sum_vec[i] = row_sum;
+		printf("\n");
+	}
+	printf("\n");
+	int calculated_index = 0;
+	for(i=0;i<n_people;i++)
+	{
+		if (col_sum_vec[i] == n_people && row_sum_vec[i] == 1) calculated_index = i;
+		printf("%d ",col_sum_vec[i]);
+	}	 
+	printf("\n   sum \n");
+
+	printf("Celebrity is located at index: %d.\n\n",calculated_index);
+	return 0;
+}
+
